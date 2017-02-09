be4fc39e8770e5bf5d79829d2310b71152b24648
zam_huang <zam_huang@mail.gss.com.tw>
2017-02-09 16:09:03 +0800

    add hook

---
+++ hooks/AddXcodeSetting.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var xcode = require('xcode');
+var path = require('path');
+
+module.exports = function(context) {
+    var projectName, pbxPath, xcodeProj;
+    var projectRoot = context.opts.projectRoot;
+
+    projectName = getConfigParser(context, path.join(projectRoot, 'config.xml')).name();
+    pbxPath = path.join(projectRoot, 'platforms', 'ios', projectName + '.xcodeproj/project.pbxproj');
+    xcodeProj = xcode.project(pbxPath);
+
+    xcodeProj.parseSync();
+    console.log(1)
+    xcodeProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
+    console.log(2)
+    // xcodeProj.addToUserHeaderSearchPaths('../../plugins/com.gss.pdfrenderer/include');
+    console.log(3)
+    xcodeProj.addToSwiftObjcBridgingHeader('$(PROJECT_DIR)/$(PROJECT_NAME)/Plugins/com.gss.pdfrenderer/PDFRenderer-Bridging-Header.h');
+    console.log(4)
+    fs.writeFileSync(pbxPath, xcodeProj.writeSync());
+    console.log(5)
+};
+
+function getConfigParser(context, config) {
+    var semver = context.requireCordovaModule('semver');
+
+    if (semver.lt(context.opts.cordova.version, '5.4.0')) {
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+    } else {
+        ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+    }
+
+    return new ConfigParser(config);
+}
